063007e3096b308a469f88787966dd64a0059475
YotaroSeki <youyoutarou.dog0115@gmail.com>
2019-04-26 17:29:36 +0900

    課題

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.titech.itpro.sdl.phototaker;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,55 +15,57 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static int REQ_PHOTO = 1234;
-    private Bitmap photoImage = null;
+  private final static int REQ_PHOTO = 1234;
+  private Bitmap photoImage = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button photoButton = findViewById(R.id.photo_button);
-        photoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-                PackageManager manager = getPackageManager();
-                List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-                if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
-                } else {
-                    Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+    Button photoButton = findViewById(R.id.photo_button);
+    photoButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // TODO: You should setup appropriate parameters for the intent
 
-    private void showPhoto() {
-        if (photoImage == null) {
-            return;
+        PackageManager manager = getPackageManager();
+        List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (!activities.isEmpty()) {
+          startActivityForResult(intent, REQ_PHOTO);
+        } else {
+          Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
         }
-        ImageView photoView = findViewById(R.id.photo_view);
-        photoView.setImageBitmap(photoImage);
-    }
+      }
+    });
+  }
 
-    @Override
-    protected void onActivityResult(int reqCode, int resCode, Intent data) {
-        super.onActivityResult(reqCode, resCode, data);
-        switch (reqCode) {
-            case REQ_PHOTO:
-                if (resCode == RESULT_OK) {
-                    // TODO: You should implement the code that retrieve a bitmap image
-                }
-                break;
+  private void showPhoto() {
+    if (photoImage == null) {
+      return;
+    }
+    ImageView photoView = findViewById(R.id.photo_view);
+    photoView.setImageBitmap(photoImage);
+  }
+
+  @Override
+  protected void onActivityResult(int reqCode, int resCode, Intent data) {
+    super.onActivityResult(reqCode, resCode, data);
+    switch (reqCode) {
+      case REQ_PHOTO:
+        if (resCode == RESULT_OK) {
+          photoImage = (Bitmap) data.getExtras().get("data");
+          // TODO: You should implement the code that retrieve a bitmap image
         }
+        break;
     }
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        showPhoto();
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    showPhoto();
+  }
 }
